88d466b2fad4312de2fbae92a0fdc6cb3dfee3ac
ValentinaZussino <vzussino@protonmail.com>
2022-10-12 15:06:42 +0200

    sistemaz esercizio 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-alert('ciao')
+// metto bottone in variabile
+const btn = document.querySelector('button');
+// creo funzione
+const stampa = function(){
+    // prendo value di input
+    let inputValue = document.getElementById('vz_number').value;
+    // se pari lo stampo
+    if(inputValue %2 == 0){
+        document.querySelector('span').innerHTML = inputValue;
+    } else { // se dispari successivo
+        inputValue ++;
+        document.querySelector('span').innerHTML = inputValue;
+    }
+    // pulisco input
+    document.getElementById('vz_number').value = '';
+}
+// on click
+btn.addEventListener('click', stampa);
