2d89bd80cf685e3e6b8b30a20c14df63358a1e35
ValentinaZussino <vzussino@protonmail.com>
2022-10-12 16:40:03 +0200

    prova impostazione es 2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
+// ESERCIZIO 1
 // metto bottone in variabile
-const btn = document.querySelector('button');
+const btn = document.getElementById('stamp');
 // creo funzione
 const stampa = function(){
     // prendo value di input
@@ -16,3 +17,21 @@ const stampa = function(){
 }
 // on click
 btn.addEventListener('click', stampa);
+
+// ESERCIZIO 2
+// creo un array per nomi, uno per cognomi, uno vuoto per ospiti
+const arrayNomi = ['nome1', 'nome2', 'nome3', 'nome4', 'nome5'];
+const arrayCognomi = ['cognome1', 'cognome2', 'cognome3', 'cognome4', 'cognome5'];
+// metto bottone in variabile 
+const btnList = document.getElementById('list');
+// genero
+const generate = function (){
+    for(let i = 0; i < 5; i++){
+    let ospiteLi = document.createElement('li');
+    let nomeOspite = arrayNomi[Math.floor(Math.random() * (arrayNomi.length))] + ' ' + arrayCognomi[Math.floor(Math.random() * (arrayCognomi.length))];
+    ospiteLi.append(nomeOspite);
+    document.querySelector('ul').append(ospiteLi)
+    }
+    
+}
+btnList.addEventListener('click', generate)
